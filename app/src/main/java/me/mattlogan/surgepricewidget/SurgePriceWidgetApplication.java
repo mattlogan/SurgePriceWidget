9ce2419cca203e73e0a79a0993520615133e235d
mattlogan <matt.b.logan@gmail.com>
2014-08-20 17:16:16 -0700

    Created tokens.xml file for server token

--- app/src/main/java/me/mattlogan/surgepricewidget/SurgePriceWidgetApplication.java
+++ app/src/main/java/me/mattlogan/surgepricewidget/SurgePriceWidgetApplication.java
@@ -8,14 +8,12 @@ import android.util.Log;
 import me.mattlogan.surgepricewidget.api.UberApi;
 import me.mattlogan.surgepricewidget.api.UberApiBus;
 import me.mattlogan.surgepricewidget.api.UberApiHandler;
-import me.mattlogan.surgepricewidget.location.CurrentLocationClient;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 
 public class SurgePriceWidgetApplication extends Application {
 
     static final String ENDPOINT = "https://api.uber.com";
-    static final String SERVER_TOKEN = "get_your_own_server_token_at_https://developer.uber.com/";
 
     UberApiHandler uberApiHandler;
 
@@ -23,19 +21,21 @@ public class SurgePriceWidgetApplication extends Application {
         Log.d("testing", "application onCreate");
         super.onCreate();
 
+        final String serverToken = getString(R.string.server_token);
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override public void run() {
-                uberApiHandler = new UberApiHandler(buildUberApi(), UberApiBus.getInstance());
+                uberApiHandler = new UberApiHandler(buildUberApi(serverToken),
+                        UberApiBus.getInstance());
             }
         });
     }
 
-    private UberApi buildUberApi() {
+    private UberApi buildUberApi(final String serverToken) {
         return new RestAdapter.Builder()
                 .setEndpoint(ENDPOINT)
                 .setRequestInterceptor(new RequestInterceptor() {
                     @Override public void intercept(RequestFacade request) {
-                        request.addQueryParam("server_token", SERVER_TOKEN);
+                        request.addQueryParam("server_token", serverToken);
                     }
                 })
                 .build()
